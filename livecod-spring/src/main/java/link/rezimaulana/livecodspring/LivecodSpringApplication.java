e0c58b00e871b61ad14f11be1d8374a39c9b8569
rezimaulana <rsazrm@gmail.com>
2022-12-11 18:52:56 +0700

    Added Spring

---
+++ livecod-spring/src/main/java/link/rezimaulana/livecodspring/LivecodSpringApplication.java
@@ -0,0 +1,13 @@
+package link.rezimaulana.livecodspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivecodSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivecodSpringApplication.class, args);
+	}
+
+}
---
+++ livecod-spring/src/test/java/link/rezimaulana/livecodspring/LivecodSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package link.rezimaulana.livecodspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivecodSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
