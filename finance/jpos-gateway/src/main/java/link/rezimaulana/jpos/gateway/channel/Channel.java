1aa21c1d350e43c72bac3977889838584f7056f7
rezimaulana <rsazrm@gmail.com>
2023-06-08 18:37:00 +0700

    Created Listener, Channel, Packager, DeployXML

---
+++ finance/jpos-gateway/src/main/java/link/rezimaulana/jpos/gateway/channel/Channel.java
@@ -0,0 +1,24 @@
+package link.rezimaulana.jpos.gateway.channel;
+
+import java.io.IOException;
+
+import org.jpos.iso.BaseChannel;
+import org.jpos.iso.ISOException;
+
+public class Channel extends BaseChannel{
+    
+    @Override
+    protected void sendMessageLength(int len) throws IOException {
+        serverOut.writeShort(len + 2);
+    }
+
+    @Override
+    protected int getMessageLength() throws IOException, ISOException {
+        int len = serverIn.readShort();
+        if(len < 2){
+            return 0;
+        }
+        return len - 2;
+    }
+
+}
